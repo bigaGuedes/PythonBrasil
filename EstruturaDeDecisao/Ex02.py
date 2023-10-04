c19be19f1ee2a34f7efc5a493f1cfffd9457f664
bigaGuedes <lgt.guedes@gmail.com>
2023-10-04 15:17:40 -0300

    ajuste nas pastas. Remoção01

--- EstruturaDeDecisao/Ex02.py
+++
@@ -1,9 +0,0 @@
-#Ex02
-#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
-valor = float(input("Insira um valor: "))
-
-if valor < 0 :
-    print("Esse valor é negativo.")
-else:
-    print("Esse valor é positivo.")    
