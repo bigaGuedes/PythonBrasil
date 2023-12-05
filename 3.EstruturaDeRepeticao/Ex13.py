cb061bbaf426052e5207267f430b88ec9d8c9a50
bigaGuedes <lgt.guedes@gmail.com>
2023-12-05 00:34:29 -0300

    Ajuste no ex13

--- 3.EstruturaDeRepeticao/Ex13.py
+++ 3.EstruturaDeRepeticao/Ex13.py
@@ -1,14 +1,12 @@
 #Ex13
 # Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
 
+
 base = int(input("Insira a base: "))
 expo = int(input("Insira o expoente: "))
-pote = 1
+potencia = 1
 
 for num in range(expo):
-    pote *= base
-    num += 1
-print(f"Resultado: {pote}")
+    potencia *= base
 
-
-
+print(f"Resultado: {potencia}")
